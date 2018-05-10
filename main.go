ebc5e7d4b99e4edd2af70c4c58cc88b454416455
Yosi Attias <yosy101@gmail.com>
2018-05-10 12:29:43 +0300

    Parameter validation and go fmt

--- main.go
+++ main.go
@@ -4,15 +4,16 @@ import (
 	"log"
 
 	"github.com/hashicorp/go-plugin"
+	"github.com/spf13/viper"
+	"github.com/streadway/amqp"
 	"github.com/victorcoder/dkron/dkron"
 	dkplugin "github.com/victorcoder/dkron/plugin"
-	"github.com/streadway/amqp"
-	"github.com/spf13/viper"
+	"strconv"
 )
 
 type rabbitMQExecutor struct {
 	conn *amqp.Connection
-	ch *amqp.Channel
+	ch   *amqp.Channel
 }
 
 func createRabbitMQExecutor() (*rabbitMQExecutor, error) {
@@ -35,21 +36,36 @@ func createRabbitMQExecutor() (*rabbitMQExecutor, error) {
 
 }
 
+func fetchConfig(config map[string]string, key string, defaultValue string) string {
+	value, ok := config[key]
+	if ok {
+		return value
+	} else {
+		return defaultValue
+	}
+}
+
 func (s *rabbitMQExecutor) Execute(args *dkron.ExecuteRequest) ([]byte, error) {
-	// TODO: validate parameters - not nil
-
 	queueName := args.Config["queue_name"]
+	log.Printf("[rabbitMQExecutor] [%s] will publish to queue [%s]\n", args.JobName, queueName)
 
-	log.Printf("[rabbitMQExecutor] running job [%s] - queue [%s]\n", args.JobName, queueName)
+	mandatory, err := strconv.ParseBool(fetchConfig(args.Config, "mandatory", "false"))
+	if err != nil {
+		log.Printf("[rabbitMQExecutor] [%s] 'mandatory' have invalid value - %v", args.JobName, err)
+	}
 
+	immediate, err := strconv.ParseBool(fetchConfig(args.Config, "immediate", "false"))
+	if err != nil {
+		log.Printf("[rabbitMQExecutor] [%s] 'immediate' have invalid value - %v", args.JobName, err)
+	}
 
-	err := s.ch.Publish(
-		"",
+	err = s.ch.Publish(
+		fetchConfig(args.Config, "exchange", ""),
 		args.Config["queue_name"],
-		false,
-		false,
+		mandatory,
+		immediate,
 		amqp.Publishing{
-			ContentType: "application/json",
+			ContentType: fetchConfig(args.Config, "header", "application/json"),
 			Body:        []byte(args.Config["payload"]),
 		})
 
@@ -60,20 +76,18 @@ func (s *rabbitMQExecutor) Execute(args *dkron.ExecuteRequest) ([]byte, error) {
 	return []byte("OK"), nil
 }
 
-
 func main() {
 
 	viper.SetDefault("rabbit_host", "amqp://guest:guest@localhost:5672/")
 
-	viper.SetConfigName("dkron-executor-rabbitmq")        // name of config file (without extension)
-	viper.AddConfigPath("/etc/dkron")   // call multiple times to add many search paths
-	viper.AddConfigPath("$HOME/.dkron") // call multiple times to add many search paths
-	viper.AddConfigPath("./config")     // call multiple times to add many search paths
-	viper.SetEnvPrefix("dkron_executor_rabbitmq")         // will be uppercased automatically
+	viper.SetConfigName("dkron-executor-rabbitmq") // name of config file (without extension)
+	viper.AddConfigPath("/etc/dkron")              // call multiple times to add many search paths
+	viper.AddConfigPath("$HOME/.dkron")            // call multiple times to add many search paths
+	viper.AddConfigPath("./config")                // call multiple times to add many search paths
+	viper.SetEnvPrefix("dkron_executor_rabbitmq")  // will be uppercased automatically
 	viper.AutomaticEnv()
 
 	viper.ReadInConfig()
-
 
 	executor, err := createRabbitMQExecutor()
 	if err != nil {
